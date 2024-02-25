4d5acc94b2d73302bc55232c655383eacc30f92f
eutrnx <134605169+eutrnx@users.noreply.github.com>
2024-02-25 13:54:52 +0000

    add new
    ehat

---
+++ .ccls-cache/@home@runner@CloudyBrownVideogames/main.cpp
@@ -0,0 +1,28 @@
+/*
+Main asslciking here
+- Write eathershit
+- Make all functions in seperate files with extension .h and then call here
+- Important to have \n, or it  new line wont print
+- Add anoteh
+*/
+
+// Including external files
+// #include "ztu/t1.h"
+// #include "ztu/t2.h"
+
+// Standar Library Calls
+// #include <iostream>
+
+// int main() {
+//   std::cout << "Hello World!\n";
+//   std::cout << "eatHrSht\n";
+// }
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  return 0;
+}
