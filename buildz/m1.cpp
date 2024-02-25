bb72fc122d354c27d10fdb3d11836f48ecc0b936
eutrnx <134605169+eutrnx@users.noreply.github.com>
2024-02-25 16:31:19 +0000

    addpussy

---
+++ buildz/m1.cpp
@@ -0,0 +1,37 @@
+/*
+Main asslciking here
+- Write eathershit
+- Make all functions in seperate files with extension .h and then call here
+- Important to have \n, or it  new line wont print
+- Add anoteh
+*/
+
+// Including external files
+
+// This is colored text pussy
+#include "ztu/t1.h"
+
+// Asks favnum and printsit 
+#include "ztu/favnum.h"
+
+
+// Standar Library Calls
+// #include <iostream>
+
+// int main() {
+//   std::cout << "Hello World!\n";
+//   std::cout << "eatHrSht\n";
+// }
+
+using namespace std;
+
+int main() {
+
+  // t1.h - color text printing
+  Pussy();
+
+  // favnum.h
+  favNum();
+
+  
+}
