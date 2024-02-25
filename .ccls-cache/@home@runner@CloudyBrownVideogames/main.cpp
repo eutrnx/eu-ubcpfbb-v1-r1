5d3e4a4cb46a9adbcfe3910e72078e02eeee34ec
eutrnx <134605169+eutrnx@users.noreply.github.com>
2024-02-25 07:20:38 +0000

    def

--- .ccls-cache/@home@runner@CloudyBrownVideogames/main.cpp
+++ .ccls-cache/@home@runner@CloudyBrownVideogames/main.cpp
@@ -1,8 +1,11 @@
+/*
+This basic file was made by the user:, when learning , drink much woman piss, then make necessary files 
+*/
+
 #include <iostream>
 
 int main() {
   std::cout << "Hello World!\n";
 }
 
-// Whats - Running some more here
 
