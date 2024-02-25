7953db27417afcdd0c91b107e690f4c5ab1a2e2d
eutrnx <134605169+eutrnx@users.noreply.github.com>
2024-02-25 07:21:28 +0000

    try add

---
+++ .ccls-cache/@home@runner@CloudyBrownVideogames/t1.cpp
@@ -0,0 +1,11 @@
+/*
+This basic file was made by the user:, when learning , drink much woman piss, then make necessary files 
+*/
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\n";
+}
+
+
