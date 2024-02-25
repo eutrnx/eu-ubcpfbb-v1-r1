19b7cbae5a59ef7e92f3ea8597a38d30fc3b23e2
eutrnx <134605169+eutrnx@users.noreply.github.com>
2024-02-25 17:10:49 +0000

    New
    - adding multipussy
    - eatwomanpoo

---
+++ tezt/mullin.h
@@ -0,0 +1,17 @@
+/*
+This header file is just for how to write multiple line text
+in cpp , while inserting nose is your womans ass and snorting her farts
+*/
+
+#ifndef MULLIN_H
+#define MULLIN_H
+
+#include <iostream>
+
+void multLine() {
+  std::cout << R"(
+    These are the multiple lines text
+  )";
+}
+
+#endif // MULLIN_H
