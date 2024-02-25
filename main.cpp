c7098a9e536b767e8cfd681e50cf32595ab89752
eutrnx <134605169+eutrnx@users.noreply.github.com>
2024-02-25 08:28:07 +0000

    major change panty

--- main.cpp
+++ main.cpp
@@ -13,3 +13,10 @@ int main() {
   std::cout << "\033[31mThis text is red.\033[0m\n";
   std::cout << "\033[32mThis text is green.\033[0m\n";
 }
+
+// Color text here
+
+void Pussy() {
+  std::cout << "\033[31mThis text is red.\033[0m\n";
+  std::cout << "\033[32mThis text is green.\033[0m\n";
+}
--- ztu/f1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "\033[31mThis text is red.\033[0m\n";
-  std::cout << "\033[32mThis text is green.\033[0m\n";
-  // More colors and formatting...
-
-  return 0;
-}
