a7019e2df82b656d88b3d3c494e35f7b26d4c2e6
eutrnx <134605169+eutrnx@users.noreply.github.com>
2024-02-25 07:49:15 +0000

    new

--- .ccls-cache/@home@runner@CloudyBrownVideogames/main.cpp
+++ .ccls-cache/@home@runner@CloudyBrownVideogames/main.cpp
@@ -1,11 +1,11 @@
 /*
-This basic file was made by the user:, when learning , drink much woman piss, then make necessary files 
+This basic file was made by the user:, when learning , drink much woman piss,
+then make necessary files
+- Keep this file and not do anything in it
+- Main work will be in subsequent cpp files, these not allowing to delte
+assandpussy
 */
 
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
-}
-
-
+int main() { std::cout << "Hello World!\n"; }
